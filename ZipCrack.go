420c8a373bf22a8f6ff52e8f6b1b86d0be099ade
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2019-08-21 18:06:34 -0400

    Update ZipCrack.go

--- ZipCrack.go
+++ ZipCrack.go
@@ -4,35 +4,51 @@ import (
     "bufio"
     "fmt"
     "os"
-    "io"
+    //"io"
+    "time"
     "log"
     "github.com/alexmullins/zip"
 )
 
 func main() {
-    r, err := zip.OpenReader("encryptedFile.zip")
+    start := time.Now()
+
+    banner := 
+    `
+    ▒███████▒ ██▓ ██▓███   ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀
+    ▒ ▒ ▒ ▄▀░▓██▒▓██░  ██▒▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ 
+    ░ ▒ ▄▀▒░ ▒██▒▓██░ ██▓▒▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ 
+      ▄▀▒   ░░██░▒██▄█▓▒ ▒▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ 
+    ▒███████▒░██░▒██▒ ░  ░▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄
+    ░▒▒ ▓░▒░▒░▓  ▒▓▒░ ░  ░░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒
+    ░░▒ ▒ ░ ▒ ▒ ░░▒ ░       ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░
+    ░ ░ ░ ░ ░ ▒ ░░░       ░          ░░   ░   ░   ▒   ░        ░ ░░ ░ 
+      ░ ░     ░           ░ ░         ░           ░  ░░ ░      ░  ░   
+    ░                     ░                           ░      
+    `
+    fmt.Println(banner)
+
+
+    fmt.Println("\nContending secretdata.zip against wordlist.txt")
+    r, err := zip.OpenReader("secretdata.zip")
     if err != nil {
         log.Fatal(err)
     }
     defer r.Close()
 
-    file, _ := os.Open("wordList.txt")
+    file, _ := os.Open("wordlist.txt")
     fscanner := bufio.NewScanner(file)
     for fscanner.Scan() {
         for _, f := range r.File {
 	    f.SetPassword(fscanner.Text())
             rc, err := f.Open()
             if err != nil {
-                //log.Printf(err.Error())
                 continue
-            }
-            _, err = io.CopyN(os.Stdout, rc, 68)
-            if err != nil {
-                log.Printf(err.Error())
             }
             rc.Close()
             r.Close()
-            fmt.Println("\n\nPassword found: ", fscanner.Text())
+            elapsed := time.Since(start)
+            log.Printf("Found password: %s in %s", fscanner.Text(), elapsed)
             os.Exit(0)
         }
     }

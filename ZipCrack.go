55342845123fc2be31718c73a02fc2d6673e9026
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2019-08-21 04:08:50 -0400

    Update ZipCrack.go

--- ZipCrack.go
+++ ZipCrack.go
@@ -3,14 +3,13 @@ package main
 import (
     "bufio"
     "fmt"
-	"os"
-	"io"
-	"log"
-	"github.com/alexmullins/zip"
+    "os"
+    "io"
+    "log"
+    "github.com/alexmullins/zip"
 )
 
 func main() {
-    // Open a zip archive for reading.
     r, err := zip.OpenReader("encryptedFile.zip")
     if err != nil {
         log.Fatal(err)
@@ -20,21 +19,21 @@ func main() {
     file, _ := os.Open("wordList.txt")
     fscanner := bufio.NewScanner(file)
     for fscanner.Scan() {
-		for _, f := range r.File {
-			f.SetPassword(fscanner.Text())
-			rc, err := f.Open()
-			if err != nil {
-				//log.Printf(err.Error())
-				continue
-			}
-			_, err = io.CopyN(os.Stdout, rc, 68)
-			if err != nil {
-				log.Printf(err.Error())
-			}
-			rc.Close()
-			r.Close()
-			fmt.Println("\n\nPassword found: ", fscanner.Text())
-			os.Exit(0)
-		}
+        for _, f := range r.File {
+	    f.SetPassword(fscanner.Text())
+            rc, err := f.Open()
+            if err != nil {
+                //log.Printf(err.Error())
+                continue
+            }
+            _, err = io.CopyN(os.Stdout, rc, 68)
+            if err != nil {
+                log.Printf(err.Error())
+            }
+            rc.Close()
+            r.Close()
+            fmt.Println("\n\nPassword found: ", fscanner.Text())
+            os.Exit(0)
+        }
     }
 } 

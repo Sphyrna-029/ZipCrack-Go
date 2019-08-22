a28dd3e945f98e6a9964b8e92ff98365c65f4c44
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2019-08-22 06:46:23 -0400

    Update ZipCrack.go

--- ZipCrack.go
+++ ZipCrack.go
@@ -5,49 +5,38 @@ import (
     "fmt"
     "os"
     "flag"
-    "time"
+    "io/ioutil"
     "log"
-    "sync"
-    "github.com/alexmullins/zip"
+    "github.com/yeka/zip"
 )
 
-var wg sync.WaitGroup 
-
-func decrypt(zipFile string, password string, passChan chan string) {
+func decrypt(zipFile string, password string) {
     r, err := zip.OpenReader(zipFile)
     if err != nil {
         log.Fatal(err)
     }
+    defer r.Close()
 	
-    f := r.File 
-    f.SetPassword(password)
-    rc, err := f.Open()
-    if err != nil {
-        close(passChan)
+    for _, f := range r.File {
+	f.SetPassword(password)
+	rc, err := f.Open()
+	if err != nil {
+	    continue
+	}
+	buf, err := ioutil.ReadAll(rc)
+	if err != nil {
+	    continue
+	}
+	rc.Close()
+	r.Close()
+	log.Printf("\nSize of %v: %v byte(s)\n", f.Name, len(buf))
+	log.Printf("============= Found password: %s =============", password)
+	os.Exit(0)
     }
-	//If solved clean up our files and send back password
-    passChan <- password
-    rc.Close()
-    r.Close()
-    close(passChan)
 }
 
 func main() {
-    start := time.Now()
 
-    //Parse cmd line arguments
-    filePtr := flag.String("zip", "", "Path to zip file.")
-    wordlistPtr := flag.String("wordlist", "", "Path to wordlist.")
-    flag.Parse()
-
-    if *filePtr == "" {
-        log.Fatal("Zip file not found.")
-    }
-    if *wordlistPtr == "" {
-        log.Fatal("Wordlist not found.")
-    }
-	
-    //Display art, info
     banner := 
     `
     ▒███████▒ ██▓ ██▓███   ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀
@@ -62,24 +51,23 @@ func main() {
     ░                     ░                           ░      
     `
     fmt.Println(banner)
-    log.Printf("Contending %s against %s", *filePtr, *wordlistPtr)
-	
-    //Initialize channel for sending solved password back to the main thread
-    passChan := make(chan string)
 
-    //Begin the main loop
+    filePtr := flag.String("zip", "", "Path to zip file.")
+    wordlistPtr := flag.String("wordlist", "", "Path to wordlist.")
+    flag.Parse()
+
+    if *filePtr == "" {
+        log.Fatal("Zip file not found.")
+    }
+    if *wordlistPtr == "" {
+        log.Fatal("Wordlist not found.")
+    }
+
+    log.Printf("Contending %s against %s", *filePtr, *wordlistPtr)
+
     file, _ := os.Open(*wordlistPtr)
     fscanner := bufio.NewScanner(file)
     for fscanner.Scan() {
-	go decrypt(*filePtr, fscanner.Text(), passChan)
-
-	passwd := <-passChan
-
-	if passwd != "" {
-	    close(passChan)
-	    elapsed := time.Since(start)
-            log.Printf("Found password: %s in %s", passwd, elapsed)
-            os.Exit(0)
-	}
+        go decrypt(*filePtr, fscanner.Text())
     }
 } 
